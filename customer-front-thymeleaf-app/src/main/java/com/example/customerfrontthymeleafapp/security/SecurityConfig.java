41d879d3add7ca3d37a150df68fa9317f3d39318
Dahmani Mohammed Karim <109095748+Karim-Dahmani@users.noreply.github.com>
2024-01-28 18:23:22 +0100

    inventory

--- customer-front-thymeleaf-app/src/main/java/com/example/customerfrontthymeleafapp/security/SecurityConfig.java
+++ customer-front-thymeleaf-app/src/main/java/com/example/customerfrontthymeleafapp/security/SecurityConfig.java
@@ -3,14 +3,23 @@ package com.example.customerfrontthymeleafapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private ClientRegistrationRepository clientRegistrationRepository;
 
@@ -24,15 +33,19 @@ public class SecurityConfig {
 
         return httpSecurity
                 .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(arg->arg.requestMatchers("/","/webjars/**","/h2-console/**").permitAll())
+                .authorizeHttpRequests(arg->arg.requestMatchers("/","/oauth2Login","/webjars/**","/h2-console/**").permitAll())
                 .authorizeHttpRequests(arg->arg.anyRequest().authenticated())
-                .oauth2Login(Customizer.withDefaults())
+//                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(al->
+                        al.loginPage("/oauth2Login")
+                                .defaultSuccessUrl("/")
+                )
                 .logout((logout) -> logout
                         .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         .logoutSuccessUrl("/").permitAll()
                          .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"))
-                //.exceptionHandling(eh->eh.accessDeniedPage("/notAutorized"))
+                .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
                 .build();
     }
     private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
@@ -40,5 +53,29 @@ public class SecurityConfig {
                 new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}?logoutsuccess=true");
         return oidcLogoutSuccessHandler;
+    }
+
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            final Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            authorities.forEach((authority) -> {
+                if (authority instanceof OidcUserAuthority oidcAuth) {
+                    mappedAuthorities.addAll(mapAuthorities(oidcAuth.getIdToken().getClaims()));
+                    System.out.println(oidcAuth.getAttributes());
+                } else if (authority instanceof OAuth2UserAuthority oauth2Auth) {
+                    mappedAuthorities.addAll(mapAuthorities(oauth2Auth.getAttributes()));
+                }
+            });
+            return mappedAuthorities;
+        };
+    }
+    private List<SimpleGrantedAuthority> mapAuthorities(final Map<String, Object> attributes) {
+        final Map<String, Object> realmAccess = ((Map<String, Object>)attributes.getOrDefault("realm_access", Collections.emptyMap()));
+        final Collection<String> roles = ((Collection<String>)realmAccess.getOrDefault("roles", Collections.emptyList()));
+        return roles.stream()
+                .map((role) -> new SimpleGrantedAuthority(role))
+                .toList();
     }
 }
--- customer-front-thymeleaf-app/src/main/java/com/example/customerfrontthymeleafapp/web/CustomerController.java
+++ customer-front-thymeleaf-app/src/main/java/com/example/customerfrontthymeleafapp/web/CustomerController.java
@@ -2,35 +2,71 @@ package com.example.customerfrontthymeleafapp.web;
 
 
 import com.example.customerfrontthymeleafapp.entities.Customer;
+import com.example.customerfrontthymeleafapp.model.Product;
 import com.example.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClient;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class CustomerController {
 
     private CustomerRepository customerRepository;
 
-    public CustomerController(CustomerRepository customerRepository) {
+    private ClientRegistrationRepository clientRegistrationRepository;
+    @Value("${inventory.service.base.uri}")
+    private String inventoryServiceBaseUri;
+    public CustomerController(CustomerRepository customerRepository, ClientRegistrationRepository clientRegistrationRepository) {
         this.customerRepository = customerRepository;
+        this.clientRegistrationRepository = clientRegistrationRepository;
     }
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String custmoers(Model model){
         List<Customer> customersList= customerRepository.findAll();
         model.addAttribute("customers",customersList);
         return "customers";
     }
     @GetMapping("/products")
-    public String products(Model model){
-        return "products";
+    public String products(Model model) {
+        try {
+            SecurityContext context = SecurityContextHolder.getContext();
+            Authentication authentication = context.getAuthentication();
+            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) authentication;
+            DefaultOidcUser oidcUser = (DefaultOidcUser) oAuth2AuthenticationToken.getPrincipal();
+            String jwtTokenValue = oidcUser.getIdToken().getTokenValue();
+            RestClient restClient = RestClient.create(inventoryServiceBaseUri);
+            List<Product> products = restClient.get()
+                    .uri("/products")
+                    .headers(httpHeaders -> httpHeaders.set(HttpHeaders.AUTHORIZATION, "Bearer " + jwtTokenValue))
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+            model.addAttribute("products", products);
+            return "products";
+        }
+        catch (Exception e){
+            return "redirect:/notAutorized";
+        }
     }
-
     @GetMapping("/auth")
     @ResponseBody
     public Authentication authentication(Authentication authentication) {
@@ -40,6 +76,24 @@ public class CustomerController {
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/notAuthorized")
+    public String notAutorized() {
+        return "notAuthorized";
+    }
+
+    @GetMapping("/oauth2Login")
+    public String oauth2Login(Model model) {
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
     }
 
 }
